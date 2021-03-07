b4c3e95f5a32dc23b03a90e608c0fd3b058af6c6
Pâyidar <79402324+payidarofficial@users.noreply.github.com>
2021-03-07 20:24:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+##############################
+# İnstall Package
+# python -m pip install colorama
+##############################
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+# Soru Cevap
+print(Fore.RED)
+print('Python Pâyidar')
